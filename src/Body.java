b5878b3ccca29352fa6a65d4f71fbf560cc4679e
AtlasGooo <1774525013@qq.com>
2021-11-29 17:52:36 +0800

    success adapt class Body to both NBodyBH and IncrementEvent

--- src/Body.java
+++ src/Body.java
@@ -14,12 +14,14 @@ public class Body {
 
     // gravitational constant
     private static final double G = 6.67e-11;
-
-    private double rx, ry;       // position
-    private double vx, vy;       // velocity
+    // (lzj) temporary change some properties to public for printing info
+    public double rx, ry;       // position
+    public double vx, vy;       // velocity
     private double fx, fy;       // force
     private double mass;         // mass
     private Color color;         // color
+
+
 
 
     // (lzj) make changes on "Body" to adapt "CollisionSystem"
@@ -30,6 +32,20 @@ public class Body {
     private int count;
     private final double radius;
 
+
+    // (lzj)
+    public Body() {
+        this.count = 0;
+
+        this.rx = StdRandom.uniform(0.5, 0.95);
+        this.ry = StdRandom.uniform(0.5, 0.95);
+        this.vx = StdRandom.uniform(-0.5, 0.5);
+        this.vy = StdRandom.uniform(-0.5, 0.5);
+        this.radius = 0.01;
+        this.mass = 0.5;
+        this.color = defaultColor;
+    }
+
     /**
      * Constructor: creates and initializes a new Body.
      *
@@ -39,11 +55,13 @@ public class Body {
      * @param vy    the y-velocity of this new body
      * @param mass  the mass of this new body
      * @param color the color of this new body (RGB)
+     * @param radius the radius of this body
      */
     public Body(double rx, double ry, double vx, double vy, double mass, Color color, double radius) {
-        // (lzj) 
+        // (lzj) add
         this.count = 0;
         this.radius = radius;
+
 
         this.rx    = rx;
         this.ry    = ry;
@@ -79,6 +97,135 @@ public class Body {
     public int count(){
         return count;
     }
+
+    // (lzj)
+    public double timeToHit(Body that){
+        if (this == that) {
+            return INFINITY;
+        }
+        double dx = that.rx - this.rx;          // x-axis displacement
+        double dy = that.ry - this.ry;          // y-axis displacement
+        double dvx = that.vx - this.vx;         // x-axis relative velocity
+        double dvy = that.vy - this.vy;         // y-axis relative velocity
+
+        // dot product of vector dr and vector dv; predicts the existence of finite time to collide
+        double dvdr = dvx * dx + dvy * dy;
+        if (dvdr > 0) {
+            return INFINITY;        // I know this physics is weird
+        }
+
+        double dvdv = dvx * dvx + dvy * dvy;    // magnitude of dv
+        if (dvdv == 0) {
+            return INFINITY;        // Means relative velocity is zero
+        }
+
+        double drdr = dx * dx + dy * dy;    // magnitude of dr vector
+
+        double sigma = that.radius + this.radius;       // sum of radii of colliding particles
+
+        // this discriminant comes from solution of a quadratic equation, see some Physics dude.
+        double discriminant = dvdr * dvdr - dvdv * (drdr - sigma * sigma);
+        if (drdr < sigma * sigma) {
+            System.out.println("Particles overlap !!! UNEXPECTED behaviour expected :p");
+        }
+        if (discriminant < 0) {
+            return INFINITY;    // there are no solutions to equation or the time to collide 
+        }
+        return -(dvdr + Math.sqrt(discriminant)) / dvdv;
+        // we ignore the other solution (Why ? because there can be two collisons in Mathematics; think about it, try it)
+    }
+
+    // (lzj)
+    public double timeToHitHorizontalWall() {
+        if (vy < 0) {
+            return (radius - ry) / vy;
+        } else if (vy > 0) {
+            return (1.0 - ry - radius) / vy;
+        } else {
+            return INFINITY;
+        }
+    }  
+
+    // (lzj)
+    public double timeToHitVerticalWall() {
+        if (vx < 0) {
+            return (radius - rx) / vx;
+        } else if (vx > 0) {
+            return (1.0 - rx - radius) / vx;
+        } else {
+            return INFINITY;
+        }
+    }
+
+    // (lzj)
+    public void bounceOff(Body that) {
+        double dx = that.rx - this.rx;
+        double dy = that.ry - this.ry;
+        double dvx = that.vx - this.vx;
+        double dvy = that.vy - this.vy;
+        // dot product of dv vector and dr vector
+        double dvdr = dvx * dx + dvy * dy;
+        // sum of the radii of this and that particle
+        double sigma = this.radius + that.radius;
+
+        // total magnitude of impulse exchanged on collision
+        double j = 2 * dvdr * this.mass * that.mass / ((this.mass + that.mass) * sigma);
+
+        // x and y components of the Impulse 
+        double jx = j * dx / sigma;
+        double jy = j * dy / sigma;
+
+        // update velocity according to momentum change given by impulse
+        this.vx += jx / this.mass;
+        this.vy += jy / this.mass;
+        that.vx -= jx / that.mass;
+        that.vy -= jy / that.mass;
+
+        // update collision counts
+        this.count++;
+        that.count++;
+    }
+
+    // (lzj)
+    public void bounceOffVerticalWall() {
+        this.vx = -this.vx;
+        count++;
+    }
+
+    // (lzj)
+    public void bounceOffHorizontalWall() {
+        this.vy = -this.vy;
+        count++;
+    }
+
+    public double kineticEnergy() {
+        // elementary formula 1/2 * m * v * v 
+        return (mass * (vx * vx + vy * vy)) / 2;
+    }
+
+    public void setDefaultColor(Color newColor) {
+        Body.defaultColor = newColor;
+    }
+
+    public static void useRadiusUpscaling(boolean yes) {
+        if (yes) {
+            useRadiusUpscaling = true;
+        } else {
+            useRadiusUpscaling = false;
+        }
+    }
+
+    public static boolean radiusUpscalingEnabled() {
+        return useRadiusUpscaling;
+    }
+
+
+
+
+
+
+
+
 
 
     /**
